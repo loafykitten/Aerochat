loafykitten/Aerochat

commit f1810d63548af192dd85ce8567e5c9705b1b6d9b

feat: Optimise nine-slice images
This commit completely overhauls the implementation of nine-slice images to
use various useful optimisation techniques. These include caching the image
set, freezing the parts, and creating the image sets before use and storing
them rather then recreating them every time the control is drawn.

This change reduces lag spikes (especially when resizing the window) and
improves the base performance of the application.

Signed-off-by: Isabella Lulamoon <kawapure@gmail.com>

Aerochat/Controls/NineSlice.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using Aerochat.Controls.NineSliceStuff;
+using NAudio.CoreAudioApi;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -6,9 +8,14 @@ using System.Windows.Media.Imaging;
 
 namespace Aerochat.Controls
 {
+    /// <summary>
+    /// A standard nine-slice image background control. The corners of the image are kept proportional, and the centre of
+    /// the image is stretched to fill its container.
+    /// </summary>
     public class NineSlice : Control
     {
-        private static readonly Action EmptyDelegate = delegate { };
+        protected NineSliceImageSet? _imageSet = null;
+
         public ImageSource? Image
         {
             get { return (ImageSource)GetValue(ImageProperty); }
@@ -22,6 +29,7 @@ namespace Aerochat.Controls
         {
             if (d is NineSlice control)
             {
+                control.ReloadImage();
                 control.InvalidateVisual();
             }
         }
@@ -39,14 +47,57 @@ namespace Aerochat.Controls
         {
             if (d is NineSlice control)
             {
+                control.ReloadImage();
                 control.InvalidateVisual();
             }
         }
 
-        private void DrawImage(DrawingContext context, BitmapSource image, Int32Rect sourceRect, Rect destRect)
+        // Virtual so that child classes can modify the implementation to fit their needs.
+        protected virtual void ReloadImage()
         {
-            CroppedBitmap croppedBitmap = new CroppedBitmap(image, sourceRect);
-            context.DrawImage(croppedBitmap, destRect);
+            _imageSet = null;
+
+            if (Image == null || !(Image is BitmapSource bitmapSource) || bitmapSource.PixelWidth == 0 || bitmapSource.PixelHeight == 0)
+            {
+                return;
+            }
+
+            _imageSet = NineSliceCacheManager.Instance.FindOrCreateImageSet(bitmapSource, Slice);
+        }
+
+        // Common static method for NineSliceButton's sake.
+        protected static CroppedBitmap? ResolvePartBitmapFromImageSet(NineSliceImageSet? imageSet, NineSlicePart part)
+        {
+            return part switch
+            {
+                NineSlicePart.TopLeft => imageSet?.TopLeft,
+                NineSlicePart.TopCenter => imageSet?.TopCenter,
+                NineSlicePart.TopRight => imageSet?.TopRight,
+                NineSlicePart.CenterLeft => imageSet?.CenterLeft,
+                NineSlicePart.CenterCenter => imageSet?.CenterCenter,
+                NineSlicePart.CenterRight => imageSet?.CenterRight,
+                NineSlicePart.BottomLeft => imageSet?.BottomLeft,
+                NineSlicePart.BottomCenter => imageSet?.BottomCenter,
+                NineSlicePart.BottomRight => imageSet?.BottomRight,
+
+                _ => throw new NotImplementedException("What the hell??"), // Shut up linter
+            };
+        }
+
+        // Virtual so that child classes can modify the implementation to fit their needs.
+        protected virtual CroppedBitmap? ResolvePartBitmap(NineSlicePart part)
+        {
+            return ResolvePartBitmapFromImageSet(_imageSet, part);
+        }
+
+        protected void DrawImage(DrawingContext context, NineSlicePart part, Rect destRect)
+        {
+            CroppedBitmap? targetBitmap = ResolvePartBitmap(part);
+
+            if (targetBitmap != null)
+            {
+                context.DrawImage(targetBitmap, destRect);
+            }
         }
 
         protected override void OnRender(DrawingContext drawingContext)
@@ -59,15 +110,16 @@ namespace Aerochat.Controls
                 drawingContext.DrawRectangle(Brushes.Transparent, null, new Rect(0, 0, ActualWidth, ActualHeight));
                 return;
             }
-            DrawImage(drawingContext, bitmapSource, new Int32Rect(0, 0, (int)Math.Max(Slice.Width, 0), (int)Math.Max(Slice.Height, 0)), new Rect(0, 0, Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0)));
-            DrawImage(drawingContext, bitmapSource, new Int32Rect((int)Math.Max(Slice.Width, 0), 0, (int)Math.Max(Image.Width - Slice.Width * 2, 0), (int)Math.Max(Slice.Height, 0)), new Rect(Math.Max(Slice.Width, 0), 0, Math.Max(ActualWidth - Slice.Width * 2, 0), Math.Max(Slice.Height, 0)));
-            DrawImage(drawingContext, bitmapSource, new Int32Rect((int)Math.Max(Image.Width - Slice.Width, 0), 0, (int)Math.Max(Slice.Width, 0), (int)Math.Max(Slice.Height, 0)), new Rect(Math.Max(ActualWidth - Slice.Width, 0), 0, Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0)));
-            DrawImage(drawingContext, bitmapSource, new Int32Rect(0, (int)Math.Max(Slice.Height, 0), (int)Math.Max(Slice.Width, 0), (int)Math.Max(Image.Height - Slice.Height * 2, 0)), new Rect(0, Math.Max(Slice.Height, 0), Math.Max(Slice.Width, 0), Math.Max(ActualHeight - Slice.Height * 2, 0)));
-            DrawImage(drawingContext, bitmapSource, new Int32Rect((int)Math.Max(Slice.Width, 0), (int)Math.Max(Slice.Height, 0), (int)Math.Max(Image.Width - Slice.Width * 2, 0), (int)Math.Max(Image.Height - Slice.Height * 2, 0)), new Rect(Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0), Math.Max(ActualWidth - Slice.Width * 2, 0), Math.Max(ActualHeight - Slice.Height * 2, 0)));
-            DrawImage(drawingContext, bitmapSource, new Int32Rect((int)Math.Max(Image.Width - Slice.Width, 0), (int)Math.Max(Slice.Height, 0), (int)Math.Max(Slice.Width, 0), (int)Math.Max(Image.Height - Slice.Height * 2, 0)), new Rect(Math.Max(ActualWidth - Slice.Width, 0), Math.Max(Slice.Height, 0), Math.Max(Slice.Width, 0), Math.Max(ActualHeight - Slice.Height * 2, 0)));
-            DrawImage(drawingContext, bitmapSource, new Int32Rect(0, (int)Math.Max(Image.Height - Slice.Height, 0), (int)Math.Max(Slice.Width, 0), (int)Math.Max(Slice.Height, 0)), new Rect(0, Math.Max(ActualHeight - Slice.Height, 0), Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0)));
-            DrawImage(drawingContext, bitmapSource, new Int32Rect((int)Math.Max(Slice.Width, 0), (int)Math.Max(Image.Height - Slice.Height, 0), (int)Math.Max(Image.Width - Slice.Width * 2, 0), (int)Math.Max(Slice.Height, 0)), new Rect(Math.Max(Slice.Width, 0), Math.Max(ActualHeight - Slice.Height, 0), Math.Max(ActualWidth - Slice.Width * 2, 0), Math.Max(Slice.Height, 0)));
-            DrawImage(drawingContext, bitmapSource, new Int32Rect((int)Math.Max(Image.Width - Slice.Width, 0), (int)Math.Max(Image.Height - Slice.Height, 0), (int)Math.Max(Slice.Width, 0), (int)Math.Max(Slice.Height, 0)), new Rect(Math.Max(ActualWidth - Slice.Width, 0), Math.Max(ActualHeight - Slice.Height, 0), Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0)));
+
+            DrawImage(drawingContext, NineSlicePart.TopLeft, new Rect(0, 0, Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0)));
+            DrawImage(drawingContext, NineSlicePart.TopCenter, new Rect(Math.Max(Slice.Width, 0), 0, Math.Max(ActualWidth - Slice.Width * 2, 0), Math.Max(Slice.Height, 0)));
+            DrawImage(drawingContext, NineSlicePart.TopRight, new Rect(Math.Max(ActualWidth - Slice.Width, 0), 0, Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0)));
+            DrawImage(drawingContext, NineSlicePart.CenterLeft, new Rect(0, Math.Max(Slice.Height, 0), Math.Max(Slice.Width, 0), Math.Max(ActualHeight - Slice.Height * 2, 0)));
+            DrawImage(drawingContext, NineSlicePart.CenterCenter, new Rect(Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0), Math.Max(ActualWidth - Slice.Width * 2, 0), Math.Max(ActualHeight - Slice.Height * 2, 0)));
+            DrawImage(drawingContext, NineSlicePart.CenterRight, new Rect(Math.Max(ActualWidth - Slice.Width, 0), Math.Max(Slice.Height, 0), Math.Max(Slice.Width, 0), Math.Max(ActualHeight - Slice.Height * 2, 0)));
+            DrawImage(drawingContext, NineSlicePart.BottomLeft, new Rect(0, Math.Max(ActualHeight - Slice.Height, 0), Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0)));
+            DrawImage(drawingContext, NineSlicePart.BottomCenter, new Rect(Math.Max(Slice.Width, 0), Math.Max(ActualHeight - Slice.Height, 0), Math.Max(ActualWidth - Slice.Width * 2, 0), Math.Max(Slice.Height, 0)));
+            DrawImage(drawingContext, NineSlicePart.BottomRight, new Rect(Math.Max(ActualWidth - Slice.Width, 0), Math.Max(ActualHeight - Slice.Height, 0), Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0)));
         }
     }
 }

Aerochat/Controls/NineSliceButton.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aerochat.Controls.NineSliceStuff;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -20,36 +21,61 @@ namespace Aerochat.Controls
         Disabled
     }
 
+    /// <summary>
+    /// A variant of the nine-slice background control suited for buttons. This automatically handles the changing of the
+    /// background based on mouse interactions.
+    /// </summary>
     public class NineSliceButton : NineSlice
     {
-        private ButtonState __state = ButtonState.Normal;
-        private ButtonState _state { get => __state; set 
-            { 
-                __state = value;
-                switch (value)
-                {
-                    case ButtonState.Normal:
-                        _image = Normal;
-                        break;
-                    case ButtonState.Hover:
-                        _image = Hover;
-                        break;
-                    case ButtonState.Pressed:
-                        _image = Pressed;
-                        break;
-                }
-                InvalidateVisual();
-            } 
-        }
-        private ImageSource? _image
-        {
-            get => Image;
-            set => Image = value;
-        }
+        protected NineSliceImageSet? _hoverImageSet = null;
+        protected NineSliceImageSet? _pressedImageSet = null;
+        //protected NineSliceImageSet? _disabledImageSet = null;
 
+        private ButtonState _state = ButtonState.Normal;
+
+        // XXX isabella: These properties may only be set once and will not respond to updates. If such functionality
+        // is desired in the future, then these need to be replaced with dependency properties.
         public ImageSource? Normal { get; set; }
         public ImageSource? Hover { get; set; }
         public ImageSource? Pressed { get; set; }
+
+        protected override void ReloadImage()
+        {
+            if (Normal == null || !(Normal is BitmapSource normalBitmapSource) || normalBitmapSource.PixelWidth == 0 || normalBitmapSource.PixelHeight == 0)
+            {
+                return;
+            }
+
+            _imageSet = NineSliceCacheManager.Instance.FindOrCreateImageSet(normalBitmapSource, Slice);
+
+            if (Hover == null || !(Hover is BitmapSource hoverBitmapSource) || hoverBitmapSource.PixelWidth == 0 || hoverBitmapSource.PixelHeight == 0)
+            {
+                return;
+            }
+
+            _hoverImageSet = NineSliceCacheManager.Instance.FindOrCreateImageSet(hoverBitmapSource, Slice);
+
+            if (Pressed == null || !(Pressed is BitmapSource pressedBitmapSource) || pressedBitmapSource.PixelWidth == 0 || pressedBitmapSource.PixelHeight == 0)
+            {
+                return;
+            }
+
+            _pressedImageSet = NineSliceCacheManager.Instance.FindOrCreateImageSet(pressedBitmapSource, Slice);
+        }
+
+        protected override CroppedBitmap? ResolvePartBitmap(NineSlicePart part)
+        {
+            if (_state == ButtonState.Hover)
+            {
+                return ResolvePartBitmapFromImageSet(_hoverImageSet, part);
+            }
+            else if (_state == ButtonState.Pressed)
+            {
+                return ResolvePartBitmapFromImageSet(_pressedImageSet, part);
+            }
+
+            return ResolvePartBitmapFromImageSet(_imageSet, part);
+        }
 
         protected override void OnRender(DrawingContext drawingContext)
         {

Aerochat/Controls/ColorizedNineSlice.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aerochat.Controls.NineSliceStuff;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -8,25 +9,33 @@ using Color = System.Windows.Media.Color;
 
 namespace Aerochat.Controls
 {
+    /// <summary>
+    /// A variant of the nine-slice image background control that can be tinted to a certain colour.
+    /// 
+    /// Note that this variant of the control is exempt from caching due to the nature of tinted backgrounds, so
+    /// it should not be used for common controls which may appear repeated a lot throughout the application.
+    /// Fortunately, this isn't the case in WLM 09, but it's worth consideration from Aerochat contributors.
+    /// </summary>
     public class ColorizedNineSlice : NineSlice
     {
         public static readonly DependencyProperty TintColorProperty =
             DependencyProperty.Register(nameof(TintColor), typeof(Color), typeof(ColorizedNineSlice),
-            new FrameworkPropertyMetadata(Colors.Red, FrameworkPropertyMetadataOptions.AffectsRender));
+            new FrameworkPropertyMetadata(Colors.Red, FrameworkPropertyMetadataOptions.AffectsRender, OnBackgroundDependentPropertyChanged));
 
         public static readonly DependencyProperty DarkenProperty =
             DependencyProperty.Register(nameof(Darken), typeof(double?), typeof(ColorizedNineSlice),
-            new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender, null, CoerceDarken));
+            new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender, OnBackgroundDependentPropertyChanged, CoerceDarken));
 
         public static readonly DependencyProperty BackgroundProperty =
-    DependencyProperty.Register(nameof(Background), typeof(Brush), typeof(ColorizedNineSlice),
-    new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
+            DependencyProperty.Register(nameof(Background), typeof(Brush), typeof(ColorizedNineSlice),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender, OnBackgroundDependentPropertyChanged));
 
         public Brush Background
         {
             get { return (Brush)GetValue(BackgroundProperty); }
             set { SetValue(BackgroundProperty, value); }
         }
+
         public Color TintColor
         {
             get { return (Color)GetValue(TintColorProperty); }
@@ -47,21 +56,18 @@ namespace Aerochat.Controls
             return value.HasValue ? Math.Max(0.0, Math.Min(1.0, value.Value)) : 0.0;
         }
 
-        private void DrawImage(DrawingContext context, BitmapSource image, Int32Rect sourceRect, Rect destRect)
+        private static void OnBackgroundDependentPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            try
+            if (d is ColorizedNineSlice control)
             {
-                CroppedBitmap croppedBitmap = new CroppedBitmap(image, sourceRect);
-                context.DrawImage(croppedBitmap, destRect);
-            } catch (Exception)
-            {
-                context.DrawImage(image, destRect);
+                control.ReloadImage();
+                control.InvalidateVisual();
             }
         }
 
-        protected override void OnRender(DrawingContext drawingContext)
+        protected override void ReloadImage()
         {
-            BitmapSource bitmapSource = null;
+            BitmapSource? bitmapSource = null;
 
             // If the Image is null and Background is set, create a solid color "image"
             if (Image == null && Background != null)
@@ -74,21 +80,16 @@ namespace Aerochat.Controls
             {
                 // Use the actual image if present
                 bitmapSource = source;
-
             }
 
-            if (bitmapSource == null)
+            _imageSet = null;
+
+            if (bitmapSource != null)
             {
-                // Clear the control if both the image and background are null
-                drawingContext.DrawRectangle(Brushes.Transparent, null, new Rect(0, 0, ActualWidth, ActualHeight));
-                return;
+                // Apply tint to the image before drawing
+                BitmapSource tintedImage = ApplyTint(bitmapSource, TintColor, Darken ?? 0.0);
+                _imageSet = NineSliceImageSet.FromBitmap(tintedImage, Slice);
             }
-
-            // Apply tint to the image before drawing
-            BitmapSource tintedImage = ApplyTint(bitmapSource, TintColor, Darken ?? 0.0);
-
-            // Perform nine-slice rendering using the tinted image
-            DrawNineSliceImage(drawingContext, tintedImage);
         }
 
         private BitmapSource CreateSolidColorBitmap(Brush backgroundBrush)
@@ -178,55 +179,6 @@ namespace Aerochat.Controls
             }
 
             return (byte)(result * 255);
-        }
-
-        private void DrawNineSliceImage(DrawingContext context, BitmapSource image)
-        {
-            if (image == null) return;
-
-            // if slice width is smaller than image width or height smaller than height, just draw the img
-            if (Slice.Width >= image.PixelWidth || Slice.Height >= image.PixelHeight)
-            {
-                DrawImage(context, image, new Int32Rect(0, 0, image.PixelWidth, image.PixelHeight),
-                    new Rect(0, 0, image.PixelWidth, image.PixelHeight));
-                return;
-            }
-
-            // Same logic as in the base NineSlice class, but with the tinted image
-            DrawImage(context, image, new Int32Rect(0, 0, (int)Math.Max(Slice.Width, 0), (int)Math.Max(Slice.Height, 0)),
-                new Rect(0, 0, Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0)));
-
-            DrawImage(context, image, new Int32Rect((int)Math.Max(Slice.Width, 0), 0,
-                (int)Math.Max(image.Width - Slice.Width * 2, 0), (int)Math.Max(Slice.Height, 0)),
-                new Rect(Math.Max(Slice.Width, 0), 0, Math.Max(ActualWidth - Slice.Width * 2, 0), Math.Max(Slice.Height, 0)));
-
-            DrawImage(context, image, new Int32Rect((int)Math.Max(image.Width - Slice.Width, 0), 0,
-                (int)Math.Max(Slice.Width, 0), (int)Math.Max(Slice.Height, 0)),
-                new Rect(Math.Max(ActualWidth - Slice.Width, 0), 0, Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0)));
-
-            DrawImage(context, image, new Int32Rect(0, (int)Math.Max(Slice.Height, 0),
-                (int)Math.Max(Slice.Width, 0), (int)Math.Max(image.Height - Slice.Height * 2, 0)),
-                new Rect(0, Math.Max(Slice.Height, 0), Math.Max(Slice.Width, 0), Math.Max(ActualHeight - Slice.Height * 2, 0)));
-
-            DrawImage(context, image, new Int32Rect((int)Math.Max(Slice.Width, 0), (int)Math.Max(Slice.Height, 0),
-                (int)Math.Max(image.Width - Slice.Width * 2, 0), (int)Math.Max(image.Height - Slice.Height * 2, 0)),
-                new Rect(Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0), Math.Max(ActualWidth - Slice.Width * 2, 0), Math.Max(ActualHeight - Slice.Height * 2, 0)));
-
-            DrawImage(context, image, new Int32Rect((int)Math.Max(image.Width - Slice.Width, 0), (int)Math.Max(Slice.Height, 0),
-                (int)Math.Max(Slice.Width, 0), (int)Math.Max(image.Height - Slice.Height * 2, 0)),
-                new Rect(Math.Max(ActualWidth - Slice.Width, 0), Math.Max(Slice.Height, 0), Math.Max(Slice.Width, 0), Math.Max(ActualHeight - Slice.Height * 2, 0)));
-
-            DrawImage(context, image, new Int32Rect(0, (int)Math.Max(image.Height - Slice.Height, 0),
-                (int)Math.Max(Slice.Width, 0), (int)Math.Max(Slice.Height, 0)),
-                new Rect(0, Math.Max(ActualHeight - Slice.Height, 0), Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0)));
-
-            DrawImage(context, image, new Int32Rect((int)Math.Max(Slice.Width, 0), (int)Math.Max(image.Height - Slice.Height, 0),
-                (int)Math.Max(image.Width - Slice.Width * 2, 0), (int)Math.Max(Slice.Height, 0)),
-                new Rect(Math.Max(Slice.Width, 0), Math.Max(ActualHeight - Slice.Height, 0), Math.Max(ActualWidth - Slice.Width * 2, 0), Math.Max(Slice.Height, 0)));
-
-            DrawImage(context, image, new Int32Rect((int)Math.Max(image.Width - Slice.Width, 0), (int)Math.Max(image.Height - Slice.Height, 0),
-                (int)Math.Max(Slice.Width, 0), (int)Math.Max(Slice.Height, 0)),
-                new Rect(Math.Max(ActualWidth - Slice.Width, 0), Math.Max(ActualHeight - Slice.Height, 0), Math.Max(Slice.Width, 0), Math.Max(Slice.Height, 0)));
         }
     }
 }

Aerochat/Controls/NineSliceStuff/NineSliceCacheManager.cs
@@ -1,0 +1,61 @@
+﻿using Aerochat.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Aerochat.Controls.NineSliceStuff;
+
+/// <summary>
+/// Manages a cache for shared nine-slice images. This is useful because a lot of our nine-slice
+/// images appear several times throughout the application as they are often used for common
+/// control backgrounds.
+/// </summary>
+internal class NineSliceCacheManager
+{
+    public static NineSliceCacheManager Instance { get; private set; } = new();
+
+    private WeakDictionary<string, NineSliceImageSet> _entries = [];
+
+    /// <summary>
+    /// Attempts to find an already existing image set meeting the same parameters, or creates a new one
+    /// if no cache entry exists.
+    /// </summary>
+    /// <returns>A nine-slice image set.</returns>
+    public NineSliceImageSet FindOrCreateImageSet(BitmapSource bitmapSource, Size slice)
+    {
+        BitmapImage? bitmapImage = bitmapSource as BitmapImage;
+        string? uri = bitmapImage?.UriSource?.ToString() ?? null;
+
+        if (uri == null)
+        {
+            BitmapFrame? bitmapFrame = bitmapSource as BitmapFrame;
+            uri = bitmapFrame?.Decoder.ToString() ?? null;
+        }
+
+        if (uri is not null)
+        {
+            // If we have a URI, then we are eligible for caching.
+            uri += $"{slice.Width}x{slice.Height}";
+
+            if (_entries.TryGetValue(uri, out NineSliceImageSet? cachedResult) && cachedResult is not null)
+            {
+                return cachedResult;
+            }
+        }
+
+        NineSliceImageSet result = NineSliceImageSet.FromBitmap(bitmapSource, slice);
+
+        if (uri is not null) // Eligible for caching:
+        {
+            _entries.Add(uri, result);
+        }
+
+        return result;
+    }
+}

commit 00403a4e69ed5de33ae429db10b65e78b5e8f2f1

fix: Nine-slice buttons without normal image (i.e. on chat window)
Buttons on the chat window (i.e. along the top row) use nine-slice buttons
while explicitly not stating a normal state image, as they are intended to
have a transparent background when not hovered. This broke along with
f1810d63548af192dd85ce8567e5c9705b1b6d9b.

Signed-off-by: Isabella Lulamoon <kawapure@gmail.com>

Aerochat/Controls/NineSlice.cs
@@ -25,7 +25,7 @@ namespace Aerochat.Controls
         public static readonly DependencyProperty ImageProperty =
             DependencyProperty.Register("Image", typeof(ImageSource), typeof(NineSlice), new PropertyMetadata(default(ImageSource), OnImageChanged));
 
-        private static void OnImageChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        protected static void OnImageChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is NineSlice control)
             {
@@ -104,7 +104,13 @@ namespace Aerochat.Controls
         {
             base.OnRender(drawingContext);
 
-            if (Image == null || !(Image is BitmapSource bitmapSource) || bitmapSource.PixelWidth == 0 || bitmapSource.PixelHeight == 0)
+            // For NineSliceButton: Instead of using the Image property to make sure that the image is not transparent,
+            // we'll just test that we have a top-left fragment. If we do, then we certainly have other fragments as
+            // well. If we were to test Image, then this wouldn't work for nine-slice buttons where the default state
+            // is transparent (no image) but there is another state.
+            CroppedBitmap? testingFragment = ResolvePartBitmap(NineSlicePart.TopLeft);
+
+            if (testingFragment == null || !(testingFragment is BitmapSource bitmapSource) || bitmapSource.PixelWidth == 0 || bitmapSource.PixelHeight == 0)
             {
                 // clear the control if the image is null or empty
                 drawingContext.DrawRectangle(Brushes.Transparent, null, new Rect(0, 0, ActualWidth, ActualHeight));

Aerochat/Controls/NineSliceButton.cs
@@ -33,34 +33,49 @@ namespace Aerochat.Controls
 
         private ButtonState _state = ButtonState.Normal;
 
-        // XXX isabella: These properties may only be set once and will not respond to updates. If such functionality
-        // is desired in the future, then these need to be replaced with dependency properties.
-        public ImageSource? Normal { get; set; }
-        public ImageSource? Hover { get; set; }
-        public ImageSource? Pressed { get; set; }
+        public ImageSource? Normal
+        {
+            get { return (ImageSource)GetValue(NormalProperty); }
+            set { SetValue(NormalProperty, value); InvalidateVisual(); }
+        }
+
+        public static readonly DependencyProperty NormalProperty =
+            DependencyProperty.Register("Normal", typeof(ImageSource), typeof(NineSliceButton), new PropertyMetadata(default(ImageSource), OnImageChanged));
+
+        public ImageSource? Hover
+        {
+            get { return (ImageSource)GetValue(HoverProperty); }
+            set { SetValue(HoverProperty, value); InvalidateVisual(); }
+        }
+
+        public static readonly DependencyProperty HoverProperty =
+            DependencyProperty.Register("Hover", typeof(ImageSource), typeof(NineSliceButton), new PropertyMetadata(default(ImageSource), OnImageChanged));
+
+        public ImageSource? Pressed
+        {
+            get { return (ImageSource)GetValue(PressedProperty); }
+            set { SetValue(PressedProperty, value); InvalidateVisual(); }
+        }
+
+        public static readonly DependencyProperty PressedProperty =
+            DependencyProperty.Register("Pressed", typeof(ImageSource), typeof(NineSliceButton), new PropertyMetadata(default(ImageSource), OnImageChanged));
 
         protected override void ReloadImage()
         {
-            if (Normal == null || !(Normal is BitmapSource normalBitmapSource) || normalBitmapSource.PixelWidth == 0 || normalBitmapSource.PixelHeight == 0)
+            if (Normal != null && (Normal is BitmapSource normalBitmapSource) && normalBitmapSource.PixelWidth != 0 && normalBitmapSource.PixelHeight != 0)
             {
-                return;
+                _imageSet = NineSliceCacheManager.Instance.FindOrCreateImageSet(normalBitmapSource, Slice);
             }
 
-            _imageSet = NineSliceCacheManager.Instance.FindOrCreateImageSet(normalBitmapSource, Slice);
-
-            if (Hover == null || !(Hover is BitmapSource hoverBitmapSource) || hoverBitmapSource.PixelWidth == 0 || hoverBitmapSource.PixelHeight == 0)
+            if (Hover != null && (Hover is BitmapSource hoverBitmapSource) && hoverBitmapSource.PixelWidth != 0 && hoverBitmapSource.PixelHeight != 0)
             {
-                return;
+                _hoverImageSet = NineSliceCacheManager.Instance.FindOrCreateImageSet(hoverBitmapSource, Slice);
             }
 
-            _hoverImageSet = NineSliceCacheManager.Instance.FindOrCreateImageSet(hoverBitmapSource, Slice);
-
-            if (Pressed == null || !(Pressed is BitmapSource pressedBitmapSource) || pressedBitmapSource.PixelWidth == 0 || pressedBitmapSource.PixelHeight == 0)
+            if (Pressed != null && (Pressed is BitmapSource pressedBitmapSource) && pressedBitmapSource.PixelWidth != 0 && pressedBitmapSource.PixelHeight != 0)
             {
-                return;
+                _pressedImageSet = NineSliceCacheManager.Instance.FindOrCreateImageSet(pressedBitmapSource, Slice);
             }
-
-            _pressedImageSet = NineSliceCacheManager.Instance.FindOrCreateImageSet(pressedBitmapSource, Slice);
         }
 
         protected override CroppedBitmap? ResolvePartBitmap(NineSlicePart part)